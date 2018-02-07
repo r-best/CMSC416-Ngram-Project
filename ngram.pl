bdb06db7bc98e6859316a2e6faba72d35d39530c
r-best <bobbyisbest3@gmail.com>
2018-02-07 11:25:01 -0500

    Reworking to divide by sentences

--- ngram.pl
+++ ngram.pl
@@ -1,4 +1,5 @@
 use strict;
+use Data::Dumper;
 use List::Util qw(min max);
 
 sub println { print "@_"."\n" }
@@ -31,29 +32,33 @@ foreach my $file (@files){
         my $text = do { local $/; <$fh> }; # Read in the entire file as a string
         chomp $text;
         $text = lc $text; # Convert to lowercase
-        $text =~ s/[!\?\.]/ <end> <start> /g; # Replace !, ?, and . with sentence separation
+        $text =~ s/[!\?\.]/ <end><split><start> /g; # Replace !, ?, and . with sentence separation
         $text =~ s/([\(\)\$,'`"\x{2019}\x{201c}\x{201d}%&:;])/ $1 /g; # Separate punctuation characters into their own tokens
-        # println $text;
-
-        my @tokens = split(/[\s\n]+/, $text);
         
-        for(my $i = 0; $i < 0+@tokens; $i++){
-            # For each token, assemble the corresponding N-gram and (N-1)-gram
-            # from it and the N-1/N-2 tokens before it, respectively
-            for(my $n = $N-1; $n <= $N; $n++){
-                if($i-$n+1 < 0) { next; } # If not enough tokens before this one to make an $n -gram, skip this $n
-                my $gram = "";
-                for(my $n2 = 0; $n2 < $n; $n2++){
-                    $gram = $gram eq "" ?
-                        $tokens[$i-$n2] :
-                        $tokens[$i-$n2]." ".$gram;
+        my @sentences = split(/<split>/, $text);
+
+        foreach my $sentence (@sentences){
+            # For each sentence, go through the tokens in it
+            my @tokens = split(/[\s\n]+/, $sentence);
+            if(0+@tokens < $N-2){ next; } # If not enough tokens in this sentence (minus the <start> and <end>) then skip it
+            
+            for(my $i = 0; $i < 0+@tokens; $i++){
+                # For each token, assemble the corresponding N-gram and (N-1)-gram
+                # from it and the N-1/N-2 tokens before it, respectively
+                for(my $n = $N-1; $n <= $N; $n++){
+                    if($i-$n+1 < 0) { next; } # If not enough tokens before this one to make an $n-gram, skip this $n
+                    my $gram = "";
+                    for(my $n2 = 0; $n2 < $n; $n2++){
+                        $gram = $gram eq "" ?
+                            $tokens[$i-$n2] :
+                            $tokens[$i-$n2]." ".$gram;
+                    }
+                    $ngrams[$n]{$gram}++;
                 }
-                $ngrams[$n]{$gram}++;
             }
         }
 
-        use Data::Dumper;
-        print Dumper(@ngrams);
+        print Dumper($ngrams[1]{'bee'});
 
         close $fh;
     } else { # If unable to open file, ignore it and keep going
